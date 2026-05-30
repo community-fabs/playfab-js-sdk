adcf42dae7356f9566532fc6d6d26421a8595361
Greg Willard <gwillard@r3pwn.com>
2026-05-29 22:59:35 -0400

    test: minor refactor, add isClientLoggedIn test

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,41 +1,49 @@
 import { describe, it, expect, beforeEach, jest } from "@jest/globals";
 
 import type { PlayfabClient } from "@/core";
-import createClientClient, { LoginResult } from "../src/apis/client";
+import getClientApi from "../src/apis/client";
 import { createMockPlayfabClient } from "./mocks/core";
 
 let playfab: jest.Mocked<PlayfabClient>;
-let clientApi: ReturnType<typeof createClientClient>;
+let clientApi: ReturnType<typeof getClientApi>;
 
 describe("ClientApi", () => {
   beforeEach(() => {
     playfab = createMockPlayfabClient();
-    clientApi = createClientClient(playfab);
+    clientApi = getClientApi(playfab);
   });
 
-  it("should store auth context on login", async () => {
-    playfab.request.mockResolvedValueOnce({
-      SessionTicket: "mock_session_ticket",
-      PlayFabId: "dummy_playfab_id",
-      NewlyCreated: false,
-      EntityToken: {
-        EntityToken: "mock_entity_token",
-        TokenExpiration: "2025-06-16T22:23:51Z",
-        Entity: {
-          Id: "dummy_playfab_id",
-          Type: "title_player_account",
-          TypeString: "title_player_account",
-        },
-      }
+  describe("when logging in", () => {
+    beforeEach(async () => {
+      playfab.request.mockResolvedValueOnce({
+        SessionTicket: "mock_session_ticket",
+        PlayFabId: "dummy_playfab_id",
+        NewlyCreated: false,
+        EntityToken: {
+          EntityToken: "mock_entity_token",
+          TokenExpiration: "2025-06-16T22:23:51Z",
+          Entity: {
+            Id: "dummy_playfab_id",
+            Type: "title_player_account",
+            TypeString: "title_player_account",
+          },
+        }
+      });
+      
+      await clientApi.loginWithCustomID({
+        CustomId: "test-custom-id",
+      });
+    })
+
+    it("should store auth context", async () => {
+      expect(playfab.request).toHaveBeenCalled();
+  
+      expect(playfab.entityToken).toEqual("mock_entity_token");
+      expect(playfab.sessionTicket).toEqual("mock_session_ticket");
     });
 
-    await clientApi.loginWithCustomID({
-      CustomId: "test-custom-id",
+    it("isLoggedIn should return true", async () => {
+      expect(clientApi.isClientLoggedIn()).toBe(true);
     });
-
-    expect(playfab.request).toHaveBeenCalled();
-
-    expect(playfab.entityToken).toEqual("mock_entity_token");
-    expect(playfab.sessionTicket).toEqual("mock_session_ticket");
   });
 });
